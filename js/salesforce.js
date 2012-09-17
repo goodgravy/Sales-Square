c18dc36107df8c33068627a23afc60ce378af72d
James Brady <james.colin.brady@gmail.com>
2012-09-16 18:24:22 -0700

    better logging

--- js/salesforce.js
+++ js/salesforce.js
@@ -4,12 +4,16 @@ var salesforce = {
 	
 	login: function() {
 		forge.tabs.openWithOptions({
-			url: "https://login.salesforce.com/services/oauth2/authorize?client_id=" + salesforce.consumer_key + "&display=touch&response_type=token&redirect_uri="+encodeURIComponent("https://login.salesforce.com/services/oauth2/success"),
+			url: "https://login.salesforce.com/services/oauth2/authorize?client_id=" +
+				salesforce.consumer_key +
+				"&display=touch&response_type=token&redirect_uri=" +
+				encodeURIComponent("https://login.salesforce.com/services/oauth2/success"),
 			pattern: "https://login.salesforce.com/services/oauth2/succ*",
 			title: "Salesforce Login",
 			tint: [10,49,115,255],
 			buttonTint: [10,49,115,255]
 		}, function(data) {
+			forge.logging.debug("OAuth flow complete: "+JSON.stringify(data));
 			state.token = decodeURIComponent(data.url.split('#access_token=')[1].split('&')[0]);
 			forge.prefs.set('token', state.token); 
 			salesforce.getIdentity(decodeURIComponent(data.url.split('&id=')[1].split('&')[0]));
@@ -17,6 +21,7 @@ var salesforce = {
 	},
 	
 	getIdentity: function(url) {
+		forge.logging.debug("getIdentity: "+url);
 		forge.request.ajax({
 			url: url,
 			type: "POST",
@@ -26,8 +31,8 @@ var salesforce = {
 				"oauth_token": state.token
 			},
 			headers: {
-                "Authorization": "OAuth "+ state.token
-            },
+				"Authorization": "OAuth "+ state.token
+			},
 			success: function (data) {
 				if (typeof data == "string") {
 					data = JSON.parse(data);
@@ -38,15 +43,16 @@ var salesforce = {
 				subscribe(); //Subscribe for push notification for this organization
 			},
 			error: function(data) {
-				forge.logging.log('Error getting identity');
-				forge.logging.log(data);
+				forge.logging.error('Error getting identity: ' + data);
 			}
 		});
 	},
 	
 	getOpportunities: function() {
 		forge.request.ajax({
-			url : state.identity.urls.query.replace("{version}", "24.0") + "?q=" + encodeURI("SELECT Name FROM Opportunity where StageName='Prospecting' or StageName='Qualification'"),
+			url : state.identity.urls.query.replace("{version}", "24.0") +
+				"?q=" +
+				encodeURI("SELECT Name FROM Opportunity where StageName='Prospecting' or StageName='Qualification'"),
 			headers : {
 				'Authorization' : 'OAuth ' + state.token
 			},
@@ -61,8 +67,7 @@ var salesforce = {
 				state.opportunities = data;
 			},
 			error: function(data) {
-				forge.logging.log('Error getting opportunities');
-				forge.logging.log(data);
+				forge.logging.error('Error getting opportunities: ' + data);
 				forge.prefs.set('token', '');
 				delete state.token;
 				delete state.identity;
@@ -72,8 +77,13 @@ var salesforce = {
 	
 	post: function(msg, file) {
 		//TODO: add file upload
+		var postURL = state.identity.urls.feeds.replace("{version}", "25.0") +
+			"/news/" +
+			state.identity.user_id +
+			"/feed-items";
+		forge.logging.debug("POSTing to " + postURL);
 		forge.request.ajax({
-			url : state.identity.urls.feeds.replace("{version}", "25.0") + "/news/"+state.identity.user_id+"/feed-items",
+			url : postURL,
 			type: "POST",
 			headers : {
 				'Authorization' : 'OAuth ' + state.token
@@ -83,18 +93,17 @@ var salesforce = {
 				"text": msg
 			},
 			success : function(response) {
-				forge.logging.log('Success posting: '+msg)
+				forge.logging.info('Success posting: '+response);
 			},
 			error: function(data) {
-				forge.logging.log('ERROR posting');
-				forge.logging.log(data);
+				forge.logging.error('Error posting: '+JSON.stringify(data));
 			}
 		});
 	}
-}
+};
 
-forge.prefs.get('token', function(token) {
-	forge.prefs.get('identity', function(identity) {
+forge.prefs.get('token', function (token) {
+	forge.prefs.get('identity', function (identity) {
 		if (token && identity) {
 			state.token = token;
 			state.identity = JSON.parse(identity);
